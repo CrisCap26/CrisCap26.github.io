cd93d5c799a0cba3e11a485439076d291878dc57
Cris <cristopher.26hp@gmail.com>
2021-01-24 22:48:45 -0600

    Version mejoradadel dark-mode

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-
-
 const btnSwitch = document.querySelector('#dark');
 const btnLight = document.querySelector('#light');
 const btnBlack = document.querySelector('#black');
-let li_0 = document.querySelector('#li-0');
-let li_1 = document.querySelector('#li-1');
-let caja = document.querySelector('#box');
-let in_0 = document.querySelector('#in-0');
-let in_1 = document.querySelector('#in-1');
-let in_2 = document.querySelector('#in-2');
-
 
 btnSwitch.addEventListener('click', () => {
     //Cambiar color de fondo del body
@@ -17,27 +8,6 @@ btnSwitch.addEventListener('click', () => {
     
     btnLight.classList.toggle('encendido');
     btnBlack.classList.toggle('apagado');
-    //Cambiar color de fondo del div box
-    if(caja.classList.contains('box-style')){
-        caja.classList.add('box-dark');
-        caja.classList.remove('box-style');
-    } else {
-        caja.classList.add('box-style');
-        caja.classList.remove('box-dark');
-    }
-
-    //Cambiar color de fondo de los input
-    if(in_0.className == 'style-input' && in_1.className == 'style-input' && 
-        in_2.className == 'style-input'){
-
-        in_0.className = 'style-input-dark';
-        in_1.className = 'style-input-dark';
-        in_2.className = 'style-input-dark';
-    } else {
-        in_0.className = 'style-input';
-        in_1.className = 'style-input';
-        in_2.className = 'style-input';
-    }
 
     //Guardar el mode en LocalStorage
     if(document.body.classList.contains('dark-mode')){
@@ -53,11 +23,7 @@ const btn_menu = document.querySelector('#btn-menu');
 const icon_menu = document.querySelector('#icon-menu');
 const icon_close = document.querySelector('#close');
 const nav_menu = document.querySelector('#menu');
-/* btnLight.classList.toggle('encendido');
-btn_menu.addEventListener('click', () => {
-    icon_menu.classList.toggle('apagado');
-    icon_close.classList.toggle('encendido');
-});*/
+
 
 btn_menu.addEventListener('click', () => {
     
@@ -70,32 +36,13 @@ btn_menu.addEventListener('click', () => {
 //Obtener modo actual
 if(localStorage.getItem('modo-dark') === 'true'){
     document.body.classList.add('dark-mode');
-    
-    
     btnBlack.classList.add('apagado');
     btnLight.classList.add('encendido');
-    caja.classList.add('box-dark');
-    caja.classList.remove('box-style');
-    in_0.classList.add('style-input-dark');
-    in_0.classList.remove('style-input');
-    in_1.classList.add('style-input-dark');
-    in_1.classList.remove('style-input');
-    in_2.classList.add('style-input-dark');
-    in_2.classList.remove('style-input');
+  
 } else {
     document.body.classList.remove('dark-mode');
-    
-    
     btnBlack.classList.remove('apagado');
     btnLight.classList.remove('encendido');
-    caja.classList.remove('box-dark');
-    caja.classList.add('box-style');
-    in_0.classList.remove('style-input-dark');
-    in_0.classList.add('style-input');
-    in_1.classList.remove('style-input-dark');
-    in_1.classList.add('style-input');
-    in_2.classList.remove('style-input-dark');
-    in_2.classList.add('style-input');
 }
 
 
